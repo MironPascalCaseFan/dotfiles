551faef9082796e8d2f860da9d1b2ffb637302f4
Mirion <mironnesvetaylov@gmail.com>
2025-07-30 08:41:07 +0300

    figure out how to run tasks concurrently

--- nvim/lua/core/async.lua
+++ nvim/lua/core/async.lua
@@ -59,31 +59,63 @@ local function wait(ms)
   end
 end
 
+local function measure_time(fn, message)
+  print(message, " started")
+  local start = vim.uv.hrtime()
+  fn()
+  local elapsed = vim.uv.hrtime() - start
+  print(message, " finished: ", elapsed / 10e6, "ms | ", elapsed / 10e9, "seconds\n")
+end
+
 coroutine.wrap(function()
-  -- first example
-  local future1 = await(wait(1000))
-  local future2 = await(wait(1000))
-  print("all futures done")
+  measure_time(function()
+    local future1 = await(wait(1000))
+    local future2 = await(wait(1000))
+  end, "first example")
 
-  --second example
+  measure_time(function()
+    await(join { wait(1000), wait(1000), wait(1000), wait(1000), wait(1000) })
+  end, "1.5 example")
+
+
+  measure_time(function()
+    local fut1 = function(cb)
+      for i = 1, 10 do
+        print(string.format("hi number {%s} from the first task", i))
+        await(wait(500))
+      end
+      cb()
+    end
+
+    local fut2 = function(cb)
+      for i = 1, 5 do
+        print(string.format("hi number {%s} from the second task", i))
+        await(wait(500))
+      end
+      cb()
+    end
+
+    await(join { fut1, fut2 })
+  end, "second example")
+
   -- https://doc.rust-lang.org/book/ch17-02-concurrency-with-async.html
-  local task1 = function(cb)
-    for i = 1, 10 do
-      print(string.format("hi number {%s} from the first task", i))
-      await(wait(500))
-    end
-    cb()
-  end
+  measure_time(function()
+    local task1 = coroutine.wrap(function(cb)
+      for i = 1, 10 do
+        print(string.format("hi number {%s} from the first task", i))
+        await(wait(500))
+      end
+      cb()
+    end)
 
-  local task2 = function(cb)
-    for i = 1, 5 do
-      print(string.format("hi number {%s} from the second task", i))
-      await(wait(500))
-    end
-    cb()
-  end
-  await(join { task1, task2 })
-  print("program finished")
-  -- well, that kind of doesn't work as I thought XD :)
-  -- need to figure out how to fix it. Each future in separate coroutine or what???
+    local task2 = coroutine.wrap(function(cb)
+      for i = 1, 5 do
+        print(string.format("hi number {%s} from the second task", i))
+        await(wait(500))
+      end
+      cb()
+    end)
+
+    await(join { task1, task2 })
+  end, "third example")
 end)()
