f3f9b9a96f5dc31b9cc02c547e6c4c0bb55be5d7
Mirion <mironnesvetaylov@gmail.com>
2025-08-22 22:01:25 +0300

    finally got my js mpv player plugin working...

--- mpv/scripts/ankits/src/main.ts
+++ mpv/scripts/ankits/src/main.ts
@@ -1,4 +1,18 @@
-import "mpv-promise";
+// polyfills
+import "core-js/full/map";
+import "core-js/full/set";
+import 'core-js/es/typed-array';
+import "core-js/full/array";
+import "core-js/full/string";
+import "core-js/full/promise";
+
+import AssDraw from "mpv-assdraw";
+import { parse } from "parse5";
+import HttpClient from "HttpClient";
+let http = new HttpClient();
+let ass = new AssDraw();
+
+const document = parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>');
 
 async function sleep(t: number) {
   return new Promise((resolve) => {
