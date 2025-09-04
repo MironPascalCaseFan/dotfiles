c9cf49cc85abe93e0fcc310197ecb10a8ef6b304
Mirion <mironnesvetaylov@gmail.com>
2025-09-04 12:25:51 +0300

    setup bash DA

--- nvim/lua/languages/bash.lua
+++ nvim/lua/languages/bash.lua
@@ -1,1 +1,30 @@
 vim.lsp.enable("bashls")
+
+vim.dap.adapters.bashdb = {
+  type = 'executable';
+  command = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/bash-debug-adapter';
+  name = 'bashdb';
+}
+
+vim.dap.configurations.sh = {
+  {
+    type = 'bashdb',
+    request = 'launch',
+    name = "Launch file",
+    showDebugOutput = true,
+    pathBashdb = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+    pathBashdbLib = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+    trace = true,
+    file = "${file}",
+    program = "${file}",
+    cwd = '${workspaceFolder}',
+    pathCat = "cat",
+    pathBash = "/bin/bash",
+    pathMkfifo = "mkfifo",
+    pathPkill = "pkill",
+    args = {},
+    argsString = '',
+    env = {},
+    terminalKind = "integrated",
+  }
+}
