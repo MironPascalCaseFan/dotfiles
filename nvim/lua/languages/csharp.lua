2eb5201c0afb3fe475b9f470b51b28ca4909f019
Mirion <mironnesvetaylov@gmail.com>
2025-09-23 18:16:03 +0300

    figure out how to debug xunit tests csharp

--- nvim/lua/languages/csharp.lua
+++ nvim/lua/languages/csharp.lua
@@ -14,15 +14,27 @@ vim.dap.configurations.cs = {
       return vim.pick_file('find ./bin/Debug -type f -name "*.dll"')
     end,
   },
+  -- https://github.com/Samsung/netcoredbg/issues/188
+  -- https://github.com/Dynge/xunit-debugging-test
+  -- need to use `VSTEST_HOST_DEBUG=1 dotnet test` and then attach to this process
+  -- https://github.com/dotnet/sdk/issues/4994
+  -- Fuck microsoft though, csharp is utter garbage and its semi proprietary ecosystem
+  {
+    type = "coreclr",
+    name = "Attach",
+    request = "attach",
+    processId = vim.dap.utils.pick_process,
+    console = "integratedTerminal",
+  },
 }
 
 vim.lsp.config("roslyn_ls", {
   cmd = {
     "dotnet",
     "/home/miron/realhome/tools/roslyn/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer.dll",
-    "--logLevel",                -- this property is required by the server
+    "--logLevel",              -- this property is required by the server
     "Information",
-    "--extensionLogDirectory",   -- this property is required by the server
+    "--extensionLogDirectory", -- this property is required by the server
     vim.fs.joinpath(vim.uv.os_tmpdir(), "roslyn_ls/logs"),
     "--stdio",
   }
