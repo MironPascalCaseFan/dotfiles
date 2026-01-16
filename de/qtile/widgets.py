c712173583a3596d274070cd1f70408f5e434c8f
miron <mironnesvetaylov@gmail.com>
2026-01-16 15:55:25 +0300

    fix qtile config after update... Fuck arch linux. Need to install nix...

--- de/qtile/widgets.py
+++ de/qtile/widgets.py
@@ -1,10 +1,8 @@
 from libqtile.widget import base
 import subprocess
 
-from libqtile.widget import base
-import subprocess
 
-class SingBoxStatus(base.ThreadPoolText):
+class SingBoxStatus(base.InLoopPollText):
     defaults = [
         ("update_interval", 5, "How often to check (seconds)"),
         ("active_text", "Proxy ON", "Text when sing-box is running"),
@@ -15,10 +13,9 @@ class SingBoxStatus(base.ThreadPoolText):
 
     def __init__(self, **config):
         super().__init__("", **config)
-        self.add_defaults(SingBoxStatus.defaults)
+        self.add_defaults(self.defaults)
 
     def poll(self):
-        """Check if sing-box is running and return status text"""
         try:
             subprocess.check_output(["pgrep", "-f", "sing-box"])
             return self.active_text
@@ -26,17 +23,14 @@ class SingBoxStatus(base.ThreadPoolText):
             return self.inactive_text
 
     def button_press(self, x, y, button):
-        """Toggle sing-box on left-click"""
-        self.toggle_proxy()
+        if button == 1:
+            self.toggle_proxy()
 
     def toggle_proxy(self):
-        """Toggle proxy by checking process state directly"""
         try:
-            # If any sing-box process exists, stop it
             subprocess.check_output(["pgrep", "-f", "sing-box"])
             subprocess.Popen(self.stop_cmd)
         except subprocess.CalledProcessError:
-            # If no process found, start it
             subprocess.Popen(self.start_cmd)
-        
-        self._update()  # refresh immediately
+
+        self.tick()  # force refresh
